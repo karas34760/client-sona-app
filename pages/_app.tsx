8e3a272b6f2dfd899d6e419a960e9cf5cd6ac031
kafierence <vannam34760@gmail.com>
2023-08-17 23:04:38 +0700

    Init Struct

---
+++ pages/_app.tsx
@@ -0,0 +1,26 @@
+import theme from '@/themes/theme';
+import { ChakraProvider } from '@chakra-ui/react';
+import { AppProps } from 'next/app';
+import 'nprogress/nprogress.css';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      {/* <style jsx global>
+        {`
+          :root {
+            --font-public: ${public_sans.style.fontFamily};
+          }
+        `}
+      </style> */}
+
+      <ChakraProvider theme={theme}>
+        <DefaultLayout>
+          <Component {...pageProps} />
+        </DefaultLayout>
+      </ChakraProvider>
+    </>
+  );
+}
+
+export default App;
---
+++ themes/theme.tsx
@@ -0,0 +1,16 @@
+import {
+  ComponentStyleConfig,
+  ThemeConfig,
+  extendTheme,
+} from '@chakra-ui/react';
+
+const config: ThemeConfig = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+};
+const theme = extendTheme({
+  
+  config,
+});
+
+export default theme;
