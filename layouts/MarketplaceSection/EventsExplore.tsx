99788dedcb35d90f14ea984b9fe530d0c58c685c
kafierence <vannam34760@gmail.com>
2023-09-19 10:42:51 +0700

    Update Discount Ticket Section

--- layouts/MarketplaceSection/EventsExplore.tsx
+++ layouts/MarketplaceSection/EventsExplore.tsx
@@ -1,17 +1,16 @@
 import { Box, Container, HStack, Text } from '@chakra-ui/react';
 import React from 'react';
-
+// su kien theo the loai
 const EventsExplore = () => {
+  const ListEvents = [];
   return (
     <Container maxWidth="container.xl">
-      <Text variant="type_sub_title">Explorer</Text>
+      <Text variant="type_sub_title">Events</Text>
       <Text>
         Lorem ipsum dolor sit amet consectetur. Iaculis vestibulum purus
         facilisi ultrices sed faucibus proin cum ut.
       </Text>
-      <HStack width="full" justifyContent="space-between">
-        <Box>dd</Box>
-      </HStack>
+      <HStack width="full" justifyContent="space-between"></HStack>
     </Container>
   );
 };
