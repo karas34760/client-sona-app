b2a3ac080aa62a3e222d559a0a8ba0bab6fa10cb
kafierence <vannam34760@gmail.com>
2023-11-05 21:39:50 +0700

    [fix] format fill form

--- graphql/query.ts
+++ graphql/query.ts
@@ -1,12 +1,6 @@
 /* eslint-disable no-unused-vars */
 import { gql } from 'graphql-request';
 
-const getHello = gql`
-  query GetHello {
-    getHello
-  }
-`;
-
 const SearchConnectMsg = gql`
   mutation SearchConnectMsg($address: String!) {
     searchConnectMsg(address: $address) {
