57931c1db690cca2482ef8b8c2df656cc4336c31
kafierence <vannam34760@gmail.com>
2023-11-09 00:29:14 +0700

    [fix]

--- graphql/client.ts
+++ graphql/client.ts
@@ -1,10 +1,14 @@
 import { GraphQLClient } from 'graphql-request';
 
+import { getAccessToken } from '@/redux/user/user-helper';
 import { GRAPHQL_END_POINT } from 'codegen';
-
+const token = getAccessToken();
 const client = new GraphQLClient(GRAPHQL_END_POINT, {
   mode: 'cors',
   timeout: 60_000,
+  headers: {
+    Authorization: token ? `Bearer ${token}` : '',
+  },
 });
 
 export default client;
