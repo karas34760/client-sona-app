3f1f041c550a3287840076b3190daca1c599beb1
kafierence <vannam34760@gmail.com>
2023-12-20 23:48:55 +0700

    [update] sitemap

--- layouts/Event/EventTab/Oganizer.tsx
+++ layouts/Event/EventTab/Oganizer.tsx
@@ -32,22 +32,17 @@ const Oganizer = ({ organizer }: IProps) => {
       <Box>
         {data.searchAccountByAddress.profile && (
           <HStack gap={10}>
-            <Box
-              borderRadius="xl"
-              overflow="hidden"
-              height="150px"
-              width="150px"
-            >
-              {data.searchAccountByAddress.profile.avatar && (
-                <Image
-                  alt=""
-                  src={data.searchAccountByAddress.profile.avatar}
-                  objectFit="cover"
-                  height="full"
-                  width="full"
-                />
-              )}
-            </Box>
+            {data.searchAccountByAddress.profile.avatar && (
+              <Image
+                alt=""
+                src={data.searchAccountByAddress.profile.avatar}
+                objectFit="contain"
+                height="150px"
+                width="150px"
+                borderRadius="xl"
+              />
+            )}
+
             <Box>
               <Flex alignItems="center" gap={2}>
                 <Text color={colorText} fontSize="xl" fontWeight="bold">
@@ -57,13 +52,7 @@ const Oganizer = ({ organizer }: IProps) => {
                   <VerifySymbol />
                 )}
               </Flex>
-              <Text textOverflow="clip" mt={2} noOfLines={2}>
-                This limited series of Midnight Society Access Passes grants the
-                holder studio-specific perks including but not limited to: a
-                one-of-a-kind PFP (profile pic) with unique VisorCortex, Call
-                Sign, and other attributes of various rarity. Founders are
-                entitled to voting rights on game features....
-              </Text>
+              <Text textOverflow="clip" mt={2} noOfLines={3}></Text>
             </Box>
           </HStack>
         )}
